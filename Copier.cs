9f0c50008f33b5aac02fb93d5916e8e88cb73c99
Alexander Rao <alexhrao@gmail.com>
2017-09-06 10:29:16 +0200

    Add Copier

---
+++ Copier.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Copier
+{
+    public class Copier
+    {
+        public static void Main(string[] args)
+        {
+            // Copy each to the TravelPlans
+        }
+    }
+}
